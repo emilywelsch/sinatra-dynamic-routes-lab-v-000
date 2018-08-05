d6db7fb93a2646bc0dbde2e77ecc8c9771789c0e
Emily Welsch <emwelsch@gmail.com>
2018-08-05 16:33:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     @num1 = params[:number1]
     @num2 = params[:number2]
     if params[:operation] = "add"
-      @num1.to_i + @num2.to_i
+      sum(@num1.to_i,@num2.to_i)
     else
     end
   end
