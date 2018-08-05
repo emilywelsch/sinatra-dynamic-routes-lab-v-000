60a4507fc0e72cf9575935e963fd7c9296bb09f8
Emily Welsch <emwelsch@gmail.com>
2018-08-05 16:21:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,14 @@ class App < Sinatra::Base
     "#{@number.to_i*@number.to_i}"
   end
 
-  # get '/say/:number/:phrase' do
-  #   @number = params[:number]
-  #   @phrase = params[:phrase]
-  #   "#{@number.to_i.times{|@phrase| '' += @phrase}}"
-  # end
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    output = ''
+    "#{@number.to_i.times do
+    output += @phrase}"
+    output
+  end
 
   get '/say/:number/:phrase' do
     answer = ''
