8dde41c3776ce84d56f16ca74f18fe4c68f1ba29
Emily Welsch <emwelsch@gmail.com>
2018-08-05 16:28:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,4 +24,9 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    if params[:operation] = "add"
+      "#{params[:number1]} + #{params[:number2]}"
+  end
+
 end
